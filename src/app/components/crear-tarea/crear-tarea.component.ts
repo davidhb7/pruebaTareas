31f02557a008a21f649f39b4447f833bd55a69b6
davidhb7 <herrera17david@gmail.com>
2024-09-30 11:12:56 -0500

    Funcionalidades base crear tarea

--- src/app/components/crear-tarea/crear-tarea.component.ts
+++ src/app/components/crear-tarea/crear-tarea.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators, FormArray, FormControl } from '@angular/forms';
 import { ITarea } from '../../core/interfaces/ITarea';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-crear-tarea',
@@ -9,12 +10,21 @@ import { ITarea } from '../../core/interfaces/ITarea';
   standalone:true,
   imports:[
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
   ]
 })
-export class CrearTareaComponent {
+
+export class CrearTareaComponent implements OnInit{
   //OBJETOS - CLASES
   formGroupTareas!: FormGroup;
+  arrayPersonas: any[] = [];
+  objePersonas:{
+    nombreCompleto: '',
+    edad: 18,
+    habilidades: []
+  }[]=[]
+
   tareas: ITarea[] = [
     {
       id: 0,
@@ -31,42 +41,104 @@ export class CrearTareaComponent {
     }
   ];
 
-
-  fecha: Date = new Date();
-
-
-  //VARIABLES
-  fechaHoy:string=''
+  public nuevaHabilidad: FormControl = new FormControl('', Validators.required );
 
   constructor(
-    private formBuilderTareas: FormBuilder
-  ){
-    this.fechaHoy=`${this.fecha.getFullYear()}/${this.fecha.getMonth() + 1}/${this.fecha.getDate()}`;
+    private fb: FormBuilder
+  ){ }
 
+  ngOnInit(): void {
+    this.buildForm();
+  }
 
-    this.formGroupTareas = this.formBuilderTareas.group({
-      tareas: this.formBuilderTareas.array(this.tareas['map']((tarea: ITarea) => this.crearTareaFormGroup(tarea))),
+  buildForm(): void {
+    this.formGroupTareas = this.fb.group({
+      nombreTarea: ['', Validators.required],
+      fechaTarea: ['', Validators.required],
+      personas: this.fb.array([
+        this.fb.group({
+          nombreCompleto: ['', Validators.required],
+          edad: ['', [Validators.required, Validators.min(1)]],
+          habilidades: this.fb.array([])
+        }),
+      ])
     });
+  }
+
+  get personas() {
+    return this.formGroupTareas.get('personas') as FormArray;
+  }
+
+  getHabilidades(indexPersona: number): FormArray {
+    return this.personas.at(indexPersona).get('habilidades') as FormArray;
+  }
+
+  addHabilidad(indexPersona: number) {
+    // console.log(this.nuevaHabilidad.value)
+    const habilidad = this.nuevaHabilidad.value; // Obtener el valor del input
+    if (habilidad) {
+      // Acceder al FormArray de habilidades de la persona correspondiente y agregar la nueva habilidad
+      this.getHabilidades(indexPersona).push(this.fb.control(habilidad, Validators.required));
+      // console.log(this.getHabilidades(indexPersona).value)
+      this.nuevaHabilidad.reset(); // Limpiar el campo de input
+    }
+  }
+
+  addPersona(): void {
+    const personasControl = this.formGroupTareas.get('personas') as FormArray;
+    if (personasControl) {
+      // Agrega la última persona en el FormArray a arrayPersonas
+      const currentPersona = personasControl.at(personasControl.length - 1).value; // Obtiene el último FormGroup como objeto
+      this.arrayPersonas.push(currentPersona); // Agrega la persona como un objeto
+      // Reinicia el FormArray de personas y las habilidades de la última persona
+      this.personas.reset();
+      this.resetHabilidades(personasControl.length - 1);
+
+      console.log(this.arrayPersonas); // Muestra el array actualizado
+    } else {
+      console.error('El FormArray de personas no está definido.');
+    }
+  }
+
+  eliminarHabilidad(indexPersona: number, indexHabilidad: number) {
+    this.getHabilidades(indexPersona).removeAt(indexHabilidad);
+  }
+
+  resetHabilidades(indexPersona: number) {
+    const personasControl = this.formGroupTareas.get('personas') as FormArray;
+
+    if (personasControl && personasControl.length > indexPersona) {
+      const habilidadesArray = personasControl.at(indexPersona).get('habilidades') as FormArray;
+      habilidadesArray.clear(); // Vacía el FormArray de habilidades
+    }
+  }
+
+  guardarTarea() : void {
+    const bodyRequest: ITarea = {
+      id:4,
+      nombreTarea: this.formGroupTareas.get('nombreTarea')?.value || undefined ,
+      fechaLimite: this.formGroupTareas.get('fechaTarea')?.value || undefined ,
+      estado:false,
+      personas: this.arrayPersonas
+    }
+    console.log(this.arrayPersonas)
+    this.arrayPersonas=[];
+    console.log(bodyRequest);
+  }
+
+  getCamposArray(objetoPersona: any): string[] {
+    return Object.keys(objetoPersona);
+  }
+
+
+  // Método para eliminar una persona en función de su índice
+  eliminarPersona(index: number): void {
+    this.arrayPersonas.splice(index, 1);
   }
 
 
 
-  // FormGroup y Validators de tarea
-  crearTareaFormGroup(tarea: ITarea): FormGroup {
-    return this.formBuilderTareas.group({
-      nombreTarea: [tarea.nombreTarea, Validators.required],
-      personas: this.formBuilderTareas.array(tarea.personas.map(persona => this.crearPersonaFormGroup(persona))),
-      fechaLimite: [this.fechaHoy, Validators.required],
-      estado: [tarea.estado, Validators.requiredTrue],
-    });
-  }
 
-  crearPersonaFormGroup(persona: { nombreCompleto: string; edad: number; habilidades: string[] }): FormGroup {
-    return this.formBuilderTareas.group({
-      nombreCompleto: [persona.nombreCompleto, [Validators.required, Validators.minLength(5)]],
-      edad: [persona.edad, [Validators.required, Validators.min(18), Validators.pattern('[0-9]*'), Validators.min(2)]],
-      habilidades: [persona.habilidades.join(', '), Validators.required, Validators.min(1), Validators.length],
-    });
-  }
+
 
 }
